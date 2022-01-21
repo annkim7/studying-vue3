5faf83d4499cf69fd7a143638a2f2f2ee52ca4fd
annkim7@github.com <ilc7748@gmail.com>
2022-01-21 20:50:09 +0900

    studying vue3 - 02

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Post :data="datas[i]" v-for="(a,i) in datas" :key="a"/>
+        <!-- <Post/>
+        <Post/> -->
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'container',
+    props : {
+        datas : Array,
+    },
+    components: {
+        Post,
+    }
+}
+</script>
+
+<style>
+
+</style>
